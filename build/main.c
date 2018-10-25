96e696773b95188bc8544bdcbdd909804f085d07
matanui159 <josh@minteronline.com>
2018-10-25 14:49:04 +1000

    [libau] Added log modules

--- build/main.c
+++ build/main.c
@@ -3,12 +3,16 @@
 #include <au/test.h>
 #include <au/log.h>
 
+#ifndef WHEEL_LOG_LEVEL
+	#define WHEEL_LOG_LEVEL AU_LOG_NONE
+#endif
+
 int main(int argc, char* argv[]) {
-	au_log_init(AU_LOG_DBUG, NULL, NULL);
+	au_log_init(WHEEL_LOG_LEVEL, NULL, NULL);
 	au_test_run();
 
 	if (argc < 3) {
-		au_log(AU_LOG_FAIL, "Usage: build <input> <output>");
+		au_log(AU_LOG_FAIL, "USAGE", "build <input> <output>");
 	}
 	return 0;
 }
